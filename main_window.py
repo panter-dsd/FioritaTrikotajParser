21ce9ffbddaccea69392fa52965832ce8518054c
konnov <konnov@simicon.com>
2015-03-29 19:05:57 +0300

    StayOnTop + paste button.

--- main_window.py
+++ main_window.py
@@ -2,14 +2,17 @@
 __author__ = 'konnov@simicon.com'
 
 
-from PyQt4 import QtGui
+from PyQt4 import QtCore, QtGui
 
 from page_parser import PageParser
 
 
 class MainWindow(QtGui.QMainWindow):
     def __init__(self):
-        super().__init__()
+        super().__init__(None, QtCore.Qt.WindowStaysOnTopHint)
+
+        self._paste_button = QtGui.QPushButton("Paste url")
+        self._paste_button.clicked.connect(self.on_paste)
 
         self._url_edit = QtGui.QLineEdit(self)
         self._url_edit.editingFinished.connect(self.work)
@@ -17,6 +20,7 @@ class MainWindow(QtGui.QMainWindow):
         self._text_view = QtGui.QPlainTextEdit(self)
 
         layout = QtGui.QVBoxLayout()
+        layout.addWidget(self._paste_button)
         layout.addWidget(self._url_edit)
         layout.addWidget(self._text_view)
 
@@ -24,6 +28,10 @@ class MainWindow(QtGui.QMainWindow):
         central_widget.setLayout(layout)
 
         self.setCentralWidget(central_widget)
+
+    def on_paste(self):
+        self._url_edit.setText(QtGui.QApplication.clipboard().text())
+        self.work()
 
     def work(self):
         page_parser = PageParser(self._url_edit.text())
