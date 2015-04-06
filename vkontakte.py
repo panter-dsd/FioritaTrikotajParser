46d57ea0dce38d764322cc1c3e60f451bf5710e5
konnov <konnov@simicon.com>
2015-04-06 21:54:36 +0300

    vk: add uppload photo.

--- vkontakte.py
+++ vkontakte.py
@@ -13,17 +13,21 @@ class Vkontakte(QtGui.QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        self._image_data = bytes()
+
         self._vkapi = vk.API(app_id="4856961",
                              user_login="xxx",
                              user_password="xxx",
                              access_token="dPwY2jTJ5Tpwe4ipMch4",
-                             scope="photos,friends,groups,stats,wall,messages,ads,offline")
+                             scope="photos,friends,groups,stats")
 
         self._group_edit = QtGui.QComboBox(self)
         self._group_edit.activated.connect(self._on_group_changed)
 
         self._album_edit = QtGui.QComboBox(self)
-        self._album_edit.activated.connect(self._on_album_changed)
+
+        self._upload_photo_button = QtGui.QPushButton("Upload", self)
+        self._upload_photo_button.clicked.connect(self._upload_photo_to_selected_album)
 
         self._comment_edit = QtGui.QPlainTextEdit(self)
 
@@ -32,6 +36,7 @@ class Vkontakte(QtGui.QWidget):
         layout = QtGui.QVBoxLayout()
         layout.addWidget(self._group_edit)
         layout.addWidget(self._album_edit)
+        layout.addWidget(self._upload_photo_button)
         layout.addWidget(self._comment_edit)
         layout.addWidget(self._image_preview)
 
@@ -43,6 +48,8 @@ class Vkontakte(QtGui.QWidget):
         self._comment_edit.setPlainText(comment)
 
     def set_image(self, image_data: bytes):
+        self._image_data = image_data
+
         image = QtGui.QImage()
         image.loadFromData(image_data, "JPG")
         image = image.scaledToWidth(
@@ -67,26 +74,32 @@ class Vkontakte(QtGui.QWidget):
 
         self._album_edit.setCurrentIndex(-1)
 
-    def _on_album_changed(self, index):
-        album = self._album_edit.itemData(index)
-        group = self._group_edit.itemData(index)
-        upload_url = \
-        self._vkapi.photos.getUploadServer(album_id=album, group_id=group)[
-            "upload_url"]
+    def _group_id(self):
+        return self._group_edit.itemData(self._group_edit.currentIndex())
+
+    def _album_id(self):
+        return self._album_edit.itemData(self._album_edit.currentIndex())
+
+    def _get_upload_url(self):
+        upload_url = self._vkapi.photos.getUploadServer(
+            album_id=self._album_id(), group_id=self._group_id()
+        )["upload_url"]
         print(upload_url)
+        return upload_url
+
+    def _upload_photo_to_selected_album(self):
+        upload_url = self._get_upload_url()
         if upload_url:
             params = self._upload_photo(upload_url)
             print(params)
-            self._vkapi.photos.save(album_id=album,
-                                    group_id=group,
+            self._vkapi.photos.save(album_id=self._album_id(),
+                                    group_id=self._group_id(),
                                     server=params["server"],
                                     photos_list=params["photos_list"],
                                     hash=params["hash"],
-                                    caption="my_first_photo")
+                                    caption=self._comment_edit.toPlainText())
+
 
     def _upload_photo(self, url):
-        response = requests.post(url,
-                                 files={
-                                 'file': open('/home/panter/qwr1.png', 'rb')})
-
+        response = requests.post(url, files={'file': ("image.jpg", self._image_data)})
         return json.loads(response.text)
